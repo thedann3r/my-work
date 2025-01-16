cdbf92d03fee5d73e25780e8df504200aee521e2
dann <austindan168@gmail.com>
2025-01-16 11:57:38 +0300

    crud alchemy flask

--- phase-4-work/flask-sqlalchemy/app.py
+++ phase-4-work/flask-sqlalchemy/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
@@ -9,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db =  SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-class Student(db.Model):
+class Student(db.Model, SerializerMixin):
     __tablename__ = "students"
 
     id = db.Column(db.Integer,primary_key=True)
@@ -17,9 +18,16 @@ class Student(db.Model):
     age = db.Column(db.Integer,nullable=False)
     teacher = db.Column(db.String(20),nullable=False)
 
+    def __repr__(self):
+        return f'<Student name: {self.name} age: {self.age}>'
+
 @app.route('/')
 def Home():
-    return 'im home, again'
+    return 'im home, again and again'
+@app.route('/students')
+def get_students():
+    students = Student.query.all()
+    return jsonify([student.to_dict() for student in students])
 
 if __name__ == "__main__":
     app.run(debug = True)
